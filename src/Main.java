fd697782267e847cee7282f7f1ad788f4ac069d9
SupremeSith <woof.portocarrero@gmail.com>
2025-08-25 18:57:44 -0300

    captura e exibe informação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hellooooo");
+        System.out.print("Digite o nome do produto");
+        Scanner s = new Scanner((System.in));
+
+        String nomeProduto = s.next();
+        System.out.println(nomeProduto);
     }
 }
