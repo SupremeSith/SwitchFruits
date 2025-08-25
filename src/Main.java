46a8fab317cf005cb1c6c8e1c730f27d505dd24d
SupremeSith <woof.portocarrero@gmail.com>
2025-08-25 19:09:29 -0300

    Adiciona tabela de nomes de frutas e switch com preços

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Digite o nome do produto");
-        Scanner s = new Scanner((System.in));
+        Scanner s = new Scanner(System.in);
+        System.out.println("========= FRUTAS DISPONÍVEIS =========");
+        System.out.println("| Banana   |");
+        System.out.println("| Manga    |");
+        System.out.println("| Limão    |");
+        System.out.println("| Laranja  |");
+        System.out.println("| Maçã     |");
+        System.out.println("| Uva      |");
+        System.out.println("| Melancia |");
+        System.out.println("=======================================");
 
+        //entrada do clientee
+        System.out.print("\nDigite o nome da fruta desejada: ");
         String nomeProduto = s.next().toLowerCase();
-        System.out.println(nomeProduto);
+        System.out.println("\nVocê escolheu: " + nomeProduto);
 
+        //estrutura switch para exibir valores das frutas
         switch (nomeProduto) {
             case "banana":
-                System.out.println("R$9,50");
+                System.out.println("Preço: R$ 9,50");
                 break;
 
             case "manga":
-                System.out.println("R$12,90");
+                System.out.println("Preço: R$ 12,90");
+                break;
 
             case "limão":
-                System.out.println("R$6,90");
+                System.out.println("Preço: R$ 6,90");
+                break;
 
             case "laranja":
-                System.out.println("R$7,90");
+                System.out.println("Preço: R$ 7,90");
+                break;
+
+            case "maçã":
+                System.out.println("Preço: R$ 5,50");
+                break;
+
+            case "uva":
+                System.out.println("Preço: R$ 15,00");
+                break;
+
+            case "melancia":
+                System.out.println("Preço: R$ 25,00");
+                break;
+            default:
+                System.out.println("Produto não encontrado no nosso sistema.");
+                break;
         }
-
+        s.close();
     }
-
 }
